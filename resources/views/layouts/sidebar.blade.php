272890ea3bc447506e92a8fcf28a2aa0376a883d
fiqri-peazzy <fiqricode28@gmail.com>
2026-08-20 00:08:34 +0800

    feat: implement x-cloak for sidebar backdrop and move toggle logic to centralized layout component

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -159,6 +159,3 @@
         </nav>
     </div>
 </aside>
-
-<div x-show="$store.sidebar.isMobileOpen" @click="$store.sidebar.setMobileOpen(false)"
-    class="fixed z-50 h-screen w-full bg-gray-900/50"></div>
